0583ca1b7579415c087bf4a23d32d1978a0c82a2
Albert <albertjokelin@icloud.com>
2020-11-10 00:10:06 +0530

    Added apps.py

--- Blood_Donation_System/Blood_Donation_System/settings.py
+++ Blood_Donation_System/Blood_Donation_System/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'Blood_Donation_System.apps.SystemConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
